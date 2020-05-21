8bf6e6dc8a2795fbe4dba110723a83154adbd3a9
gsc2001 <gurkirat.singh@students.iiit.ac.in>
2020-05-22 00:57:02 +0530

    [Cleaned] Cleaned code and made functions for everthing

--- resources/js/card-render.js
+++ resources/js/card-render.js
@@ -1,41 +1,35 @@
-
+$('document').ready(function(){
+    var courses = data;
+    renderDataToSmallCards(courses);
+})
 // Creating temp data
-var ISS = {
-    name: "Introduction to Information Systems",
-    tags: ["Bekar","Fast","Bekar"],
-    rating: "1.0"
-}
-console.log(data);
-courses = data;
 
-//  Adding small cards 
-const smlCardsrc = document.getElementById("card-small").innerHTML;
-const template = Handlebars.compile(smlCardsrc);
-const dump = document.getElementById('course-cards');
-let content = '';
-let cnt = 0;
-for (let crs of courses)
-{
-    if(cnt == 0)
-    {
-        content += '<div class="row2">'
+function renderDataToSmallCards(courses) {
+    //  Adding small cards 
+    const smlCardsrc = document.getElementById("card-small").innerHTML;
+    const template = Handlebars.compile(smlCardsrc);
+    const dump = document.getElementById('course-cards');
+    let content = '';
+    let cnt = 0;
+    for (let crs of courses) {
+        if (cnt == 0) {
+            content += '<div class="row2">'
+        }
+        const context = {
+            course: crs,
+        }
+        const compiled = template(context);
+        content += compiled
+        cnt++;
+        if (cnt == 3) {
+            cnt = 0;
+            content += "</div>";
+        }
     }
-    const context = {
-        course : crs,
-    }
-    const compiled = template(context);
-    content += compiled
-    cnt ++;
-    if(cnt == 3)
-    {
-        cnt = 0;
-        content += "</div>";
-    }
-}
-if(cnt != 0)
-{
+    if (cnt != 0) {
         content += '</div>';
+    }
+    dump.innerHTML = content;
 }
-dump.innerHTML = content;
 
 // end adding small cards
