001513c009ad69e4887d37ef47f53618633c4883
Pratyush Pratap Priyadarshi <ppratap2001@gmail.com>
2020-05-24 11:34:10 +0530

    closes #53

--- resources/js/file-renderer.js
+++ resources/js/file-renderer.js
@@ -4,8 +4,20 @@ function saveAndOpenYear(value)
     window.location.href = 'year.html';
 }
 
+function saveAndOpenYearIndex(value)
+{
+    localStorage.setItem(KEY_YEAR,JSON.stringify(value));
+    window.location.href = './resources/html/year.html';
+}
+
 function saveAndOpenCourse(course_id)
 {
     localStorage.setItem(KEY_COURSE,JSON.stringify(course_id));
     window.location.href =  'course.html';
+}
+
+function saveAndOpenCourseIndex(course_id)
+{
+    localStorage.setItem(KEY_COURSE,JSON.stringify(course_id));
+    window.location.href =  './resources/html/course.html';
 }
